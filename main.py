c302cf3d7d9f75a7366424b513681bdcf5fff5cf
Evil Developer <adrulub@yandex.ru>
2024-02-10 13:47:53 +0300

    solved_fourth_problem

--- main.py
+++ main.py
@@ -25,19 +25,30 @@ def promotion():
     Присоединяйся! <br>
     '''
 
-@app.route('/image_mars')
-def image_mars():
+@app.route('/promotion_image')
+def promotion_image():
     return f"""<!doctype html>
                     <html lang="en">
                       <head>
                         <meta charset="utf-8">
-                        <title>Привет, Марс!</title>
+                        <link rel="stylesheet" type="text/css" href="{url_for('static',
+                                                                              filename='css/style.css')}" />
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <title>Колонизация</title>
                       </head>
                       <body>
                         <h1>Жди нас, Марс!</h1> <br>
                         <img src="{url_for('static', filename='img/MARS.png')}" 
                                alt="здесь должна была быть картинка, но не нашлась">
-                        <p>Вот она какая, красивая планета.</p>
+                        <span class="highlighted-text">Человечество вырастает из детства.</span> <br>
+                        <span class="highlighted-text">Человечеству мала одна планета.</span> <br>
+                        <span class="highlighted-text">Мы сделаем обитаемыми безжизненные пока планеты.</span> <br>
+                        <span class="highlighted-text">И начнем с Марса!</span> <br>
+                        <span class="highlighted-text">Присоединяйся!</span> <br>
                       </body>
                     </html>"""
 
